f062e4a6fb0da1007d95dcd49c89d9ed8b5d552d
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-11-03 13:35:28 +0400

    js1

---
+++ main.js
@@ -0,0 +1,22 @@
+const arr = [
+  {
+    image: "firstImg",
+    title: "firstDesign",
+    description: "firstDescription",
+  },
+  {
+    image: "secondImg",
+    title: "secondDesign",
+    description: "secondDescription",
+  },
+  {
+    image: "thirdImg",
+    title: "thirdDesign",
+    description: "thirdDescription",
+  },
+  {
+    image: "fourthImg",
+    title: "fourthDesign",
+    description: "fourthDescription",
+  },
+];
