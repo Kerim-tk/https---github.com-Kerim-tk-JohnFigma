ef25b15b95638a785401dff8d733de494b874c8f
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-11-03 14:38:57 +0400

    js1

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 const arr = [
   {
-    image: "firstImg",
-    title: "firstDesign",
+    image: "./img/Designing Dashboards.png",
+    title: "designingDashboard",
+    year: "2020",
+    category: "dashboard",
     description: "firstDescription",
   },
   {
-    image: "secondImg",
-    title: "secondDesign",
+    image: "./img/Vibrant Portraits of 2020.png",
+    title: "vibrantPortraitsOf2020",
+    year: "2018",
+    category: "illustration",
     description: "secondDescription",
   },
   {
-    image: "thirdImg",
-    title: "thirdDesign",
+    image: "./img/36 Days of Malayalam type.png",
+    title: "36DaysOfMalayalamType",
+    year: "2018",
+    category: "typography",
     description: "thirdDescription",
   },
   {
-    image: "fourthImg",
-    title: "fourthDesign",
+    image: "./img/36 Days of Malayalam type.png",
+    title: "36DaysOfMalayalamType",
+    year: "2018",
+    category: "typography",
     description: "fourthDescription",
   },
 ];
+
+// arr.forEach((item, index) => {
+//   const div = document.createElement("div");
+//   div.setAttribute("id", index);
+//   const title = document.createElement("p");
+//   const description = document.createElement("h2");
+//   title.innerHTML = item.title;
+//   description.innerHTML = item.description;
+//   console.log(div);
+//   console.log(index);
+//   document.getElementById("kerim").appendChild(title);
+//   document.getElementById("kerim").appendChild(description);
+// });
